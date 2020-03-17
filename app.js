0f661d466054bedeb16fcbd1f20f439c6bb2ac14
bruinen <max.machnik@gmail.com>
2020-03-17 21:05:55 +0100

    Signup endpoing added

--- app.js
+++ app.js
@@ -1,38 +1,51 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const bodyParser = require("body-parser");
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
 // Graphql
-const graphqlHttp = require("express-graphql");
-const graphqlSchema = require("./graphql/schema");
-const graphqlResolver = require("./graphql/resolver");
+const graphqlHttp = require('express-graphql');
+const graphqlSchema = require('./graphql/schema');
+const graphqlResolver = require('./graphql/resolver');
 
 const app = express();
 
 app.use(bodyParser.json());
 
+app.use((req, res, next) => {
+	res.setHeader('Access-Control-Allow-Origin', '*');
+	res.setHeader(
+		'Access-Control-Allow-Methods',
+		'GET, POST, PUT, PATCH, DELETE'
+	);
+	res.setHeader('Access-Control-Allow-Headers', '*');
+	if (req.method === 'OPTIONS') {
+		return res.sendStatus(200);
+	}
+	next();
+});
+
 app.use(
-  "/graphql",
-  graphqlHttp({
-    schema: graphqlSchema,
-    rootValue: graphqlResolver,
-    graphiql: true
-  })
+	'/graphql',
+	graphqlHttp({
+		schema: graphqlSchema,
+		rootValue: graphqlResolver,
+		graphiql: true,
+	})
 );
 
 const spinnUp = async () => {
-  try {
-    await mongoose.connect(
-      "mongodb+srv://bruinen:hRxPMYjRFhTOapq2@nodecourse-wx0jk.gcp.mongodb.net/gifts",
-      {
-        useNewUrlParser: true,
-        useUnifiedTopology: true
-      }
-    );
-    app.listen(8080);
-  } catch (error) {
-    console.log(error);
-  }
+	try {
+		await mongoose.connect(
+			'mongodb+srv://bruinen:hRxPMYjRFhTOapq2@nodecourse-wx0jk.gcp.mongodb.net/gifts',
+			{
+				useNewUrlParser: true,
+				useUnifiedTopology: true,
+			}
+		);
+		app.listen(8080);
+	} catch (error) {
+		console.log(error);
+	}
 };
 
 spinnUp();
--- graphql/resolver.js
+++ graphql/resolver.js
@@ -1,27 +1,51 @@
-const bcrypt = require("bcryptjs");
-const User = require("../models/user");
+const bcrypt = require('bcryptjs');
+const User = require('../models/user');
+const validator = require('validator');
 
 module.exports = {
-  createUser: async ({ userInput }, req) => {
-    const existingUsername = await User.findOne({
-      username: userInput.username
-    });
-    if (existingUsername) {
-      const error = new Error("Username taken");
-      throw error;
-    }
-    const existingEmail = await User.findOne({ email: userInput.email });
-    if (existingEmail) {
-      const error = new Error("Username taken");
-      throw error;
-    }
-    const hashedPassword = await bcrypt.hash(userInput.password, 12);
-    const user = new User({
-      username: userInput.username,
-      email: userInput.email,
-      password: hashedPassword
-    });
-    const createdUser = await user.save();
-    return { ...createdUser._doc, _id: createdUser._id.toString() };
-  }
+	createUser: async ({ userInput }, req) => {
+		const errors = [];
+		if (!validator.isEmail(userInput.email)) {
+			errors.push({ message: 'Invalid email' });
+		}
+		if (
+			validator.isEmpty(userInput.password) ||
+			!validator.isLength(userInput.password, { min: 5 })
+		) {
+			errors.push({
+				message: 'Password should be at least 5 characters long',
+			});
+		}
+		if (!validator.isLength(userInput.username, { min: 3, max: 30 })) {
+			errors.push({
+				message:
+					'Username should be at least 3 and maximum 30 characters long',
+			});
+		}
+		if (errors.length > 0) {
+			const errorsList = errors.map(error => error.message).join('. ');
+			const error = new Error('Invalid input. ' + errorsList);
+			throw error;
+		}
+		const existingUsername = await User.findOne({
+			username: userInput.username,
+		});
+		if (existingUsername) {
+			const error = new Error('Username taken, choose another username');
+			throw error;
+		}
+		const existingEmail = await User.findOne({ email: userInput.email });
+		if (existingEmail) {
+			const error = new Error('Email already exist, please login');
+			throw error;
+		}
+		const hashedPassword = await bcrypt.hash(userInput.password, 12);
+		const user = new User({
+			username: userInput.username,
+			email: userInput.email,
+			password: hashedPassword,
+		});
+		const createdUser = await user.save();
+		return { ...createdUser._doc, _id: createdUser._id.toString() };
+	},
 };
