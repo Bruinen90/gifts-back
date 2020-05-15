65a73d2f46652a2d6cb77009ebabb4c18674e510
Bruinen <max.machnik@gmail.com>
2020-05-15 13:49:48 +0200

    EmailsToken added to mails

--- models/User.js
+++ models/User.js
@@ -1,23 +1,28 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+const crypto = require("crypto");
 
 const UserSchema = new Schema({
-	username: {
-		type: String,
-		required: true,
-	},
-	email: {
-		type: String,
-		required: true,
-	},
-	password: {
-		type: String,
-		required: true,
-	},
-	passwordResetToken: String,
-	passwordResetTokenExpDate: Date,
-	friends: [{ type: Schema.Types.ObjectId, ref: 'User' }],
-	unsubscribed: { type: Boolean, default: false },
+    username: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    passwordResetToken: String,
+    passwordResetTokenExpDate: Date,
+    friends: [{ type: Schema.Types.ObjectId, ref: "User" }],
+    unsubscribed: { type: Boolean, default: false },
+    emailLinksToken: {
+        type: String,
+        default: crypto.randomBytes(20).toString("hex"),
+    },
 });
 
-module.exports = mongoose.model('User', UserSchema);
+module.exports = mongoose.model("User", UserSchema);
