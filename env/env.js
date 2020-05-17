01bdbac2f1b4f52449d7cc8237ffc7681d3a0fcb
bruinen <max.machnik@gmail.com>
2020-05-17 11:46:02 +0200

    Security update

--- env/env.js
+++
@@ -1,5 +0,0 @@
-exports.jwtSecret = process.env.JWT_SECRET || '#flymywaytothesunn1809';
-exports.MONGO_PASSWORD = process.env.MONGO_PASSWORD || 'hRxPMYjRFhTOapq2';
-exports.SENDGRID_API_KEY =
-	process.env.SENDGRID_API_KEY ||
-	'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';
